efbfda839638c086ba08b69ddf691888557e7144
DieNice <DailyPlanning@bk.ru>
2021-03-18 13:26:54 +1000

    created redactor of knowlage, materials, creation of materials, forms

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
 import eel
 import sys
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-# from scripts.materials import materialsload
-
-
-@eel.expose
-def materialsload():
-    env = Environment(
-        loader=FileSystemLoader('.'),
-        autoescape=select_autoescape(['html', 'xml'])
-    )
-    template = env.get_template('/templates/materials.html')
-
-    rendered_page = template.render()
-
-    with open('static/materials.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+from scripts.materials import materialsload
 
 
 if __name__ == '__main__':
